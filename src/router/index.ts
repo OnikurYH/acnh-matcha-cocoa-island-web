02f86afa15bee43284ff1922b7998d5fabd7d42d
OnikurYH <onikuryh@gmail.com>
2020-04-30 07:39:07 -0700

    Fix wrong chunk name for Map page

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,10 +13,7 @@ const routes: Array<RouteConfig> = [
     {
         path: '/map',
         name: 'Map',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Map.vue'),
+        component: () => import(/* webpackChunkName: "map" */ '../views/Map.vue'),
     },
 ];
 
